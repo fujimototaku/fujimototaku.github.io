9ef20bc820d6bb6c0e0ab6fa03d3b74ac57e8ee8
fujimototaku <10futa20@gmail.com>
2026-09-13 05:33:29 +0900

    Add offline shell for TIME

---
+++ time-machine/sw.js
@@ -0,0 +1,8 @@
+const CACHE="time-v1";
+const ASSETS=["/time-machine/","/assets/time-machine.css","/assets/time-machine.js"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
